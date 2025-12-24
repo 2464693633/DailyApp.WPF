74e1b751e79ee1021f21a685ac4a520b5248083e
yang luo <2464693633@qq.com>
2025-12-24 17:27:53 +0800

    添加项目文件。

---
+++ DailyApp.WPF/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using DailyApp.WPF.Views;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Services.Dialogs;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DailyApp.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        /// <summary>
+        /// 设置启动窗体
+        /// </summary>
+        /// <returns></returns>
+        protected override Window CreateShell()
+        {
+            
+           return Container.Resolve<MainWin>();
+        }
+        /// <summary>
+        /// 依赖注入
+        /// </summary>
+        /// <param name="containerRegistry"></param>
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+           
+        }
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            var dialog = Container.Resolve<IDialogService>();
+            dialog.ShowDialog("LoginUC", callback =>
+            {
+                if (callback.Result == ButtonResult.OK)
+                {
+                    //登录成功
+                    base.OnInitialized();
+                }
+                else
+                {
+                    //登录失败 关闭程序
+                   Environment.Exit(0);
+                    return;
+                }
+            });
+            
+        }
+       
+    }
+
+}
---
+++ DailyApp.WPF/ViewModels/LoginUCViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Prism.Commands;
+using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyApp.WPF.ViewModels
+{
+    public class LoginUCViewModel : IDialogAware
+    {
+        public string Title { get; set; } = "我的日常";
+
+        public event Action<IDialogResult> RequestClose;
+
+        public DelegateCommand LogCmm {  get; set; }
+
+        public LoginUCViewModel()
+        {
+            LogCmm = new DelegateCommand(login);
+        }
+        /// <summary>
+        /// 登录方法
+        /// </summary>
+        private void login()
+        {
+            //登录成功
+            if (RequestClose != null)
+            {
+                RequestClose(new DialogResult(ButtonResult.OK));
+            }
+        }
+
+
+        /// <summary>
+        /// 是否可以关闭对话框
+        /// </summary>
+        /// <returns></returns>
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+            
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+            
+        }
+    }
+}
