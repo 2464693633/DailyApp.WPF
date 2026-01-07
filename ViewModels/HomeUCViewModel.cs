84dd5fb8b619142a6340bb86dd205b368565ca2b
yang luo <2464693633@qq.com>
2026-01-07 16:51:20 +0800

    最新

--- ViewModels/HomeUCViewModel.cs
+++ ViewModels/HomeUCViewModel.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using DailyApp.WPF.Models;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace DailyApp.WPF.ViewModels
 {
-    class HomeUCViewModel
+    internal class HomeUCViewModel: BindableBase
     {
-    }
+		private List<StatPanelInfo> _StatPanelList;
+
+		/// <summary>
+		/// 统计面板数据
+		/// </summary>
+		public List<StatPanelInfo> StatPnelList
+		{
+			get { return _StatPanelList; }
+			set {
+				_StatPanelList = value;
+				RaisePropertyChanged();
+			}
+		}
+
+
+
+
+	}
 }
